6e6addd2f3286be1fa782bd253015617c46806e7
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-05-02 09:09:24 -0300

    setup project and set logo and title home view

---
+++ OderFood/OderFood/Feature/Home/ViewController/HomeVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  OderFood
+//
+//  Created by Grazi  Berti on 01/05/24.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    var screen = HomeScreen()
+    
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.primarybgColor
+    }
+}
+
---
+++ OderFood/OderFood/Utils/Extension/UIColor+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extension.swift
+//  OderFood
+//
+//  Created by Grazi  Berti on 01/05/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static let primarybgColor = UIColor.rgb(red: 15, green: 23, blue: 42)
+    static let secondaryBgColor = UIColor.rgb(red: 163, green: 230, blue: 53)
+    static let primaryTextColor = UIColor.rgb(red: 255, green: 255, blue: 255)
+    static let secondaryTextColor = UIColor.rgb(red: 203, green: 213, blue: 225)
+}
